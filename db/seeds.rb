3edd6d1583a27104353d99ccaa3f55dee819f806
James Vanderburg <james@vanderburg.org>
2023-11-10 03:34:55 -0600

    Add Consumable seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Consumable.find_or_create_by!(name: "Chick-Fil-A Sauce") do |c|
+  c.calories = 140
+  c.carbs_in_grams = 6
+  c.total_fat_in_grams = 13
+  c.protein_in_grams = 0
+  c.sodium_in_mg = 170
+end
+
+Consumable.find_or_create_by!(name: "Chick-Fil-A Spicy Chicken Sandwich") do |c|
+  c.calories = 450
+  c.carbs_in_grams = 45
+  c.total_fat_in_grams = 19
+  c.protein_in_grams = 28
+  c.sodium_in_mg = 1730
+end
+
+Consumable.find_or_create_by!(name: "Chick-Fil-A Waffle Fries (Medium)") do |c|
+  c.calories = 420
+  c.total_fat_in_grams = 24
+  c.carbs_in_grams = 45
+  c.protein_in_grams = 5
+  c.sodium_in_mg = 240
+end
