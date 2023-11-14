9b4045361bebad46e027df7063a598e740a71b8f
James Vanderburg <james@vanderburg.org>
2023-11-13 22:40:53 -0600

    Add "Record your weight" button if none entered

--- spec/views/daily_logs/show.html.tailwindcss_spec.rb
+++ spec/views/daily_logs/show.html.tailwindcss_spec.rb
@@ -1,15 +1,38 @@
 require "rails_helper"
 
 RSpec.describe "daily_logs/show", type: :view do
-  before(:each) do
-    assign(:daily_log, DailyLog.create!(
-      date: Date.today,
-      weight: 2.5
-    ))
+  describe "when the daily log has no weight" do
+    before(:each) do
+      assign(:daily_log, build_stubbed(
+        :daily_log,
+        date: Date.today,
+        weight: nil
+      ))
+    end
+
+    it "asks user to record their weight" do
+      render
+      expect(rendered).to match(/Record your weight/)
+    end
   end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/2.5/)
+  describe "when the daily log has a weight" do
+    before(:each) do
+      assign(:daily_log, build_stubbed(
+        :daily_log,
+        date: Date.today,
+        weight: 2.5
+      ))
+    end
+
+    it "asks the user if they want to edit their weight" do
+      render
+      expect(rendered).to match(/Edit weight/)
+    end
+
+    it "renders the weight attribute" do
+      render
+      expect(rendered).to match(/2.5/)
+    end
   end
 end
