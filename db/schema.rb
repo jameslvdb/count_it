b179a8e4c11ab4481bc67ebc006f1414d499889e
James Vanderburg <james@vanderburg.org>
2023-11-14 09:17:10 -0600

    Create User model, add bcrypt for passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_10_094558) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_14_065805) do
   create_table "consumables", force: :cascade do |t|
     t.string "name"
     t.integer "calories"
@@ -45,6 +45,13 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_10_094558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["daily_log_id"], name: "index_meals_on_daily_log_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "meal_items", "consumables"
